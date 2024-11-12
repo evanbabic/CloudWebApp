61034680a91f5fda52c61d9b0ded122acd897593
babice <babice@sheridancollege.ca>
2024-11-12 15:55:17 -0500

    frontend done for now

--- CloudInfraProject/src/components/PlaylistDisplay.jsx
+++ CloudInfraProject/src/components/PlaylistDisplay.jsx
@@ -8,28 +8,39 @@ function PlaylistDisplay( {mood} ) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        setLoading(true);
-        setError(null);
+        if (mood){
 
-        axios.get('http://localhost:5000/api/playlist?mood=${mood}')
-        .then((response) => {
-            setPlaylist(response.data.playlist);
-            setLoading(false);
-        })
-        
-    })
+            setLoading(true);
+            setError(null);
+
+            axios.get('http://localhost:5000/api/playlist?mood=${mood}')
+            .then((response) => {
+                setPlaylist(response.data.playlist);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError("Failed to load playlist. Please try again.");
+                setLoading(false);
+            })
+        }
+    }, [mood]);
 
     if (loading) return <p>Loading...</p>
-
     if (error) return <p>{error}</p>
 
     return(
         <>
             <div className="playlist-display">
                 <h3>Playlist for Mood {mood}:</h3>
+                {/* <ul>
+                    {playlist.map((song, index) => (
+                        <li key={index}>
+                            {song.title} by {song.artist}
+                        </li>
+                    ))}
+                </ul> */}
             </div>
         </>
-
     )
 }
 
